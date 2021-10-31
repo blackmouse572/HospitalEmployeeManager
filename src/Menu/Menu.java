3bdbf782b5a9f30147c6476ad4f2efb326115e4f
Hoài Linh <81068699+lilinkw@users.noreply.github.com>
2021-10-31 19:09:29 +0700

    Update employee

--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -1,10 +1,10 @@
 public class Menu {
     public static int getChoice(){
-        System.out.println("--------Doctor management---------");
-        System.out.println("1.Add doctor");
-        System.out.println("2.Update doctor");
-        System.out.println("3.Delete doctor");
-        System.out.println("4.View information");
+        System.out.println("--------Employee management---------");
+        System.out.println("1.Add employee");
+        System.out.println("2.View all employee information");
+        System.out.println("3.View employee information with the highest salary");
+        System.out.println("4.Searh employee");
         System.out.println("5.Exit");
         System.out.print("Your choice: ");
         return Validation.checkInputLimit(1,5);
