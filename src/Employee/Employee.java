61f0f4ac124c53e48b936d9c6be3e9072c3a0717
Hoài Linh <81068699+lilinkw@users.noreply.github.com>
2021-11-02 14:00:44 +0700

    Fix Employee.java

--- src/Employee/Employee.java
+++ src/Employee/Employee.java
@@ -1,16 +1,16 @@
 package Employee;
 
-import java.util.Scanner;
-
 abstract class Employee {
     private String id;
     private String name;
     private String type;
     private String phone;
     private String email;
-    private float coefficient;
-    public Employee(){};
-    public Employee(String id, String name, String type, String phone, String email, float coefficient){
+    private double coefficient;
+
+    public Employee(){}
+    
+    public Employee(String id, String name, String type, String phone, String email, double coefficient){
         this.id = id;
         this.name = name;
         this.type = type;
@@ -18,24 +18,49 @@ abstract class Employee {
         this.email = email;
         this.coefficient = coefficient;
     }
-    abstract float calSalary();
+    abstract double calSalary();
     public void display(){
-        System.out.println("ID: ");
-        System.out.println("Name: ");
-        System.out.println("Type: ");
-        System.out.println("Phone: ");
-        System.out.println("Email: ");
+        System.out.printf("%-6s",this.id);
+        System.out.printf("%-10s",this.name);
+        System.out.printf("%-10s",this.type);
+        System.out.printf("%-12s",this.phone);
+        System.out.printf("%-25s",this.email);
+        System.out.printf("%-4f",this.coefficient);
     }
-    public void add(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter ID: ");
-        id = sc.nextLine();
-        System.out.print("Enter Name: ");
-        name = sc.nextLine();
-        System.out.print("Enter Phone: ");
-        phone = sc.nextLine();
-        System.out.print("Enter Email: ");
-        email = sc.nextLine();
-        System.out.print("Enter Coefficient: ");
+//Get and Set
+    public void setId(String id){
+        this.id = id;
     }
-}
+    public String getId(){
+        return this.id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public String getType(){
+        return this.type;
+    }
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+    public String getPhone(){
+        return this.phone;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public void setCoefficient(double coefficient){
+        this.coefficient = coefficient;
+    }
+    public double getCoefficient(){
+        return this.coefficient;
+    }
