d894311c142c29159406feed5c64c8650a704d5a
Ducye <93517069+Ducye@users.noreply.github.com>
2021-11-04 15:58:17 +0700

    Update Doctor.java

--- src/Employee/Doctor.java
+++ src/Employee/Doctor.java
@@ -4,85 +4,44 @@ import java.util.Scanner;
 
 public class Doctor extends Employee {
     
-    private String major;
+    private String major
     private int level;
     private int positionAllowance;
     
+   
     @Override
-    double salaryCal() {
-        double doctorSalary = (coeffSalary * 1650000) + positionAllowance * 200000;
-        return doctorSalary;
+    public double calSalary() {
+        return super.getCoefficient() * 1650000 + positionAllowance * 200000 + level;
     }
     
-    public Doctor( int id, String name, int phone, String email, 
-                int coeffSalary, String major, int positionAllowance ) {
-        super.id = id;
-        super.name = name;
-        super.phone = phone;
-        super.email = email;
-        super.coeffSalary = coeffSalary;
+    public Doctor(String id, String name, String type, String phone, String email, double coefficient, String major, int level, int positionAllowance ) {
+        super(id,name,type,phone,email,coefficient);
         this.major = major;
+        this.level = level;
         this.positionAllowance = positionAllowance;
-
     }
     
     public Doctor() {
         super();
     }
     
-    public int getID() {
-        return id;
+    public String getmajor() {
+        return major;
     }
     
-    public String getName() {
-        return name;
+    public int getlevel() {
+        return level;
     }
-    
-    public int getPhone() {
-        return phone;
-    }
-    
-    public String getEmail() {
-        return email;
-    }
-    
-    public int getCoeffSalary() {
-        return super.coeffSalary;
-    }
-    
-    public String major() {
-        return this.major;
-    }
-    
-    public int level() {
-        return this.level;
-    }
-    public int positionAllowance() {
-        return this.positionAllowance;
+
+    public int getpositionAllowance() {
+        return positionAllowance;
     }
     
     @Override
-    public void input() {
-        super.input();
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Input major: ");
-        this.major = sc.next();
-        
-        System.out.println("Input level: ");
-        this.level = sc.nextInt();
-              
-        System.out.println("Enter Position Allowance: ");
-        this.positionAllowance = sc.nextInt();
-
-    }
-    
-    // print
-    public void print() {
-        super.print();
-        System.out.println( "Major : " + this.major );
-        System.out.println( "Position Allowance: " + this.positionAllowance );
-        System.out.println( "Level: " + this.level );
-        System.out.println( "Total salary: " + salaryCal() );
+    public void display() {
+        super.display();
+        System.out.printf("%-10s",level);
+        System.out.printf("%-4d",major);
+        System.out.printf("%-4d",positionAllowance);
     }
 }
