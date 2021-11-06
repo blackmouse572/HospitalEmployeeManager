430c32c1f772d6024a7dd4599e85baef70cc1f8b
Hoài Linh <81068699+lilinkw@users.noreply.github.com>
2021-11-06 16:05:04 +0700

    Update Doctor
    fix Display method, add getter, update constructor, update calSalary.

--- src/Employee/Doctor.java
+++ src/Employee/Doctor.java
@@ -1,47 +1,58 @@
 package Employee;
 
-import java.util.Scanner;
 
 public class Doctor extends Employee {
     
-    private String major
+    private String major;
     private int level;
     private int positionAllowance;
     
-   
     @Override
-    public double calSalary() {
-        return super.getCoefficient() * 1650000 + positionAllowance * 200000 + level;
+    double calSalary() {
+         return  (super.getCoefficient() + this.level) * 1650000 + positionAllowance;
     }
     
-    public Doctor(String id, String name, String type, String phone, String email, double coefficient, String major, int level, int positionAllowance ) {
-        super(id,name,type,phone,email,coefficient);
+    public Doctor( String id, String name,String phone, String email, float coefficient, String major, int level, int positionAllowance ) {
+       super(id, name, 1, phone, email, coefficient);
         this.major = major;
-        this.level = level;
+        this.level= level;
         this.positionAllowance = positionAllowance;
-    }
-    
-    public Doctor() {
-        super();
-    }
-    
-    public String getmajor() {
-        return major;
-    }
-    
-    public int getlevel() {
-        return level;
+
     }
 
-    public int getpositionAllowance() {
-        return positionAllowance;
+    public String getId(){return super.getId();}
+
+    public String getName() {
+        return super.getName();
+    }
+
+    public String getPhone() {
+        return super.getPhone();
+    }
+
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    public float getCoefficient() {
+        return super.getCoefficient();
     }
     
-    @Override
+    public String getMajor() {
+        return this.major;
+    }
+    
+    public int getLevel() {
+        return this.level;
+    }
+    public int getPositionAllowance() {
+        return this.positionAllowance;
+    }
+    
+
+    // print
     public void display() {
         super.display();
-        System.out.printf("%-10s",this.level);
-        System.out.printf("%-4d",this.major);
-        System.out.printf("%-4d",this.positionAllowance);
+        System.out.printf("%-11s%-6d%-12d%-10f\n",this.major,this.level,this.positionAllowance,calSalary());
     }
 }
