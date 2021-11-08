7636e610d20545552d2bc9f3adbf92b780ccc5f0
Hoài Linh <81068699+lilinkw@users.noreply.github.com>
2021-11-08 09:58:17 +0700

    Update Main.java
    Eliminate Scanner class

--- src/Menu/Main.java
+++ src/Menu/Main.java
@@ -2,11 +2,9 @@ package Menu;
 
 import Employee.EmployeeManagement;
 
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
         EmployeeManagement em = new EmployeeManagement();
 
         boolean check = true;
@@ -16,7 +14,6 @@ public class Main {
                 case 1->{
                     System.out.print("press 1 to add Doctor or 2 to add Nurse: ");
                     int choice = Validation.checkInputIntLimit(1,2);
-                    String con;
                     do{
                         if(choice == 1){
                             em.addDoctor();
@@ -24,8 +21,7 @@ public class Main {
                             em.addNurse();
                         }
                         System.out.print("Do you want to continue(Y/N)? ");
-                        con = sc.next();
-                    }while(con.equals("Y") || con.equals("y") );
+                    }while(Validation.checkInputYN());
                 }
                 case 2-> em.viewAll();
                 case 3-> em.viewHighestSalary();
